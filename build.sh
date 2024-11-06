faa1e68423dcae9495b21e7b75dac2cbc875aa3a
Wenjie Wang <ww107587@gmail.com>
2024-11-07 00:09:42 +0800

    update

---
+++ build.sh
@@ -0,0 +1,3 @@
+make ARCH=arm zynq_7020_defconfig
+make ARCH=arm CROSS_COMPILE=/home/wang/toolchains/gcc-linaro-7.4.1-2019.02-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-  -j 30
+#make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf-  -j 30
